413d67780396a07471cd8e467f84c2ed495cb40d
Saulo Guimarães <saulogg00@gmail.com>
2025-05-07 21:07:16 -0400

    Alterações para configurar GithubPages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Maranatha-PETVIP', // Substitua pelo nome do seu repositório
+  base: '/Maranatha-PETVIP/', // Substitua pelo nome do seu repositório
 });
